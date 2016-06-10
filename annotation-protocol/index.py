50bdc2962b660d173319dde652ab64c7c282e5bb
BigBlueHat <byoung@bigbluehat.com>
2016-06-10 13:41:12 -0400

    Add Container variations; implement Prefer header

--- annotation-protocol/index.py
+++ annotation-protocol/index.py
@@ -31,7 +31,26 @@ default_headers = load_headers_from_file(doc_root
 @wptserve.handlers.handler
 def collection(request, response):
     """Annotation Collection"""
-    requested_file = doc_root + 'annotations/index-preferminimal.jsonld'
+
+    prefer_header = request.headers.get('Prefer');
+
+    # Prefer header variants
+    prefer_minimal = 'return=representation;include="http://www.w3.org/ns/ldp#PreferMinimalContainer"'
+    prefer_contained_uris = 'return=representation;include="http://www.w3.org/ns/ldp#PreferContainedIRIs"'
+    prefer_contained_descriptions = 'return=representation;include="http://www.w3.org/ns/oa#PreferContainedDescriptions"'
+
+    # Default Container format SHOULD be PreferContainedDescriptions
+    requested_file = doc_root + \
+        'annotations/index-PreferContainedDescriptions.jsonld'
+
+    if prefer_header:
+        if prefer_header == prefer_minimal:
+            requested_file = doc_root + \
+                'annotations/index-PreferMinimalContainer.jsonld'
+        elif prefer_header == prefer_contained_uris:
+            requested_file = doc_root + \
+                'annotations/index-PreferContainedIRIs.jsonld'
+
     populate_headers(requested_file, response)
 
     with open(requested_file) as data_file:
