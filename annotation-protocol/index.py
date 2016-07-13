bccf6f9c50f0aac868d59552006c2080c58ecfdc
BigBlueHat <byoung@bigbluehat.com>
2016-07-13 12:19:07 -0400

    Fix CORS header setting line

--- annotation-protocol/index.py
+++ annotation-protocol/index.py
@@ -218,7 +218,7 @@ def single_options(request, response):
     else:
         response.status = 404
 
-    response.header.set('Access-Control-Allow-Origin', '*')
+    response.headers.set('Access-Control-Allow-Origin', '*')
     response.content = None
 
 
