76d67acdf41e1a0a79edb87b8fe40dbdd3327e62
Shane McCarron <halindrome@gmail.com>
2016-09-08 10:53:58 -0500

    Initial implementation of ATTA support
    Example ATTA in tools directory that relies upon WPT
    ATTAcomm.js will be included in ARIA tests.
    
    Example test forthcoming.

---
+++ wai-aria/tools/atta-example.py
@@ -0,0 +1,241 @@
+# atta-example
+#
+# a skeletal implementation of an Accessible Technology Test Adapter
+#
+# Developed by Benjamin Young (@bigbulehat) and Shane McCarron (@halindrome).
+# Sponsored by Spec-Ops (https://spec-ops.io)
+#
+# Copyright (c) 2016 Spec-Ops
+#
+# for license information, see http://www.w3.org/Consortium/Legal/2008/04-testsuite-copyright.html
+
+import os
+import sys
+
+here = os.path.abspath(os.path.split(__file__)[0])
+repo_root = os.path.abspath(os.path.join(here, os.pardir, os.pardir))
+
+sys.path.insert(0, os.path.join(repo_root, "tools"))
+sys.path.insert(0, os.path.join(repo_root, "tools", "six"))
+sys.path.insert(0, os.path.join(repo_root, "tools", "html5lib"))
+sys.path.insert(0, os.path.join(repo_root, "tools", "wptserve"))
+sys.path.insert(0, os.path.join(repo_root, "tools", "pywebsocket", "src"))
+sys.path.insert(0, os.path.join(repo_root, "tools", "py"))
+sys.path.insert(0, os.path.join(repo_root, "tools", "pytest"))
+sys.path.insert(0, os.path.join(repo_root, "tools", "webdriver"))
+
+import hashlib
+import json
+import urlparse
+
+import wptserve
+
+myAPI = 'WAIFAKE'
+myAPIversion = 0.1
+myprotocol = 'http'
+myhost = 'localhost'
+port = 4119
+doc_root = os.path.join(repo_root, "wai-aria", "tools", "files")
+
+URIroot = myprotocol + '://' + myhost + ':{0}'.format(port)
+
+# testName is a test designation from the testcase
+testName = ""
+# testWindow should be a handle to the window under test while a test is running
+testWindow = None
+
+def dump_json(obj):
+    return json.dumps(obj, indent=4, sort_keys=True)
+
+def add_cors_headers(resp):
+    headers_file = doc_root + '/cors.headers'
+    resp.headers.update(load_headers_from_file(headers_file))
+
+def load_headers_from_file(path):
+    headers = []
+    with open(path, 'r') as header_file:
+        data = header_file.read()
+        headers = [tuple(item.strip() for item in line.split(":", 1))
+                   for line in data.splitlines() if line]
+    return headers
+
+def get_params(request, params):
+    resp = { "error": "" }
+
+    # loop over params and attempt to retrieve values
+    # return the values in a response dictionary
+    # 
+    # if there is an error, return it in the error member of the response
+
+    for item in params:
+        try:
+            if (request.method == "GET"):
+                val = request.GET.first(item)
+            elif (request.method == "POST"):
+                val = request.POST.first(item)
+            else:
+                resp['error'] += "Incorrect request method " + request.method + "; "
+                break
+            resp[item] = val
+        except:
+            resp['error'] += "No such parameter: " + item + "; "
+
+    return resp
+
+@wptserve.handlers.handler
+def head(request, response):
+    response.status = 200
+
+    add_cors_headers(response)
+    response.headers.update(load_headers_from_file(doc_root + '/aria.headers'))
+
+    response.content = None
+
+
+@wptserve.handlers.handler
+def options(request, response):
+    add_cors_headers(response)
+    response.headers.update(load_headers_from_file(doc_root + '/aria.headers'))
+
+    response.status = 200
+
+    add_cors_headers(response)
+    response.headers.update(load_headers_from_file(doc_root + '/aria.headers'))
+    response.headers.update("Content-Type", "text/plain")
+
+    response.content = "ATTA Options\n";
+
+
+@wptserve.handlers.handler
+def runTests(request, response):
+    
+    runResp = {
+            "status":     "OK",
+            "statusText": "",
+            "results":    []
+            }
+
+    params = get_params(request, [ 'id', 'data' ])
+
+    if (params[error] == ""):
+        # we got the input we wanted
+
+        # element to be examined is in the id parameter
+        # data to check is in the data parameter
+
+        theTests = {} 
+
+        try:
+            theTests = json.loads(params[data])
+
+            # loop over each item and update the results
+
+            for assertion in theTests:
+                # evaluate the assertion
+                runResp['results'].append({ result: "PASS", message: ""})
+
+        except:
+            # there is an error
+            runResp['status'] = "ERROR"
+            runResp['statusText'] += "Failed to decode " + params[data] + " as JSON"
+
+    else:
+        runResp['status'] = "ERROR"
+        runResp['statusTest'] = params[error]
+
+    add_cors_headers(response)
+    response.headers.update(load_headers_from_file(doc_root + '/aria.headers'))
+    response.status = 200
+    response.content = dump_json(runResp)
+
+@wptserve.handlers.handler
+def startTest(request, response):
+    method = request.method
+
+    testResp = {
+            "status": "READY",
+            "statusText": "",
+            "ATTAname":    "WPT Sample ATTA",
+            "ATTAversion": 1,
+            "API":         myAPI,
+            "APIversion":  myAPIversion
+            }
+
+    params = get_params(request, [ 'test', 'url' ])
+
+    if (params['error'] == ""):
+        # we got the input we wanted
+
+        # look for a window that talks about URL
+
+        try:
+            # do nothing
+            testResp['status'] = "READY"
+            testName = params['test']
+            # this would be a REAL A11Y reference 
+            testWindow = params['url']
+
+            print "Starting test '" + testName + "' at url '" + testWindow + "'"
+
+        except:
+            # there is an error
+            testResp['status'] = "ERROR"
+            testResp['statusText'] += "Failed to find window for " + params.url + " as JSON"
+
+    else:
+        testResp['status'] = "ERROR"
+        testResp['statusTest'] = params['error']
+
+    add_cors_headers(response)
+    response.headers.update(load_headers_from_file(doc_root + '/aria.headers'))
+    response.status = 200
+
+    if (method != "HEAD"):
+        response.content = dump_json(testResp)
+    else:
+        response.content = "ATTA Head\n"
+        response.headers.update("Content-Type", "text/plain")
+
+@wptserve.handlers.handler
+def endTest(request, response):
+    method = request.method
+    base = os.path.basename(request.request_path[1:])
+    requested_file = doc_root + request.request_path[1:]
+
+    resp  = {
+            "status": "DONE",
+            "statusText": "",
+            }
+    testName = ""
+    testWindow = None
+
+    add_cors_headers(response)
+    response.headers.update(load_headers_from_file(doc_root + '/aria.headers'))
+    response.status = 200
+    response.content = dump_json(testResp)
+
+if __name__ == '__main__':
+    print 'http://' + myhost + ':{0}/'.format(port)
+
+    routes = [
+        ("HEAD", "", head),
+        ("GET", "", wptserve.handlers.file_handler),
+        ("GET", "index.html", wptserve.handlers.file_handler),
+
+        # start a test
+        ("POST", "start", startTest),
+        ("GET", "start", startTest),
+        ("HEAD", "start", startTest),
+
+        # perform an individual test
+        ("POST", "test/", runTests),
+        ("GET", "test/", runTests),
+
+        # end testing
+        ("GET", "end", endTest),
+        ("POST", "end", endTest),
+    ]
+
+    httpd = wptserve.server.WebTestHttpd(host=myhost, bind_hostname=myhost, port=port, doc_root=doc_root,
+                                         routes=routes)
+    httpd.start(block=True)
