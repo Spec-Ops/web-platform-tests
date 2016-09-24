5e3d55576b045272a84ffad47b0f7018e4cd0da9
Shane McCarron <halindrome@gmail.com>
2016-09-24 14:22:59 +0100

    Added command line arguments and usage. Added support for embedded reference links and descriptions.

--- wai-aria/tools/make_tests.pl
+++ wai-aria/tools/make_tests.pl
@@ -1,71 +1,135 @@
 #!/usr/bin/perl
 #
-#  make-tests.pl - generate WPT test cases from the testable statements wiki
+#  make_tests.pl - generate WPT test cases from the testable statements wiki
 #
+#  This script assumes that a wiki has testable statement entries
+#  in the format described by the specification at 
+#  https://spec-ops.github.io/atta-api/index.html
 #
+#  usage: make_tests.pl -f file | -w wiki_title | -s spec -d dir
 
 use strict;
 
 use IO::String ;
 use JSON ;
 use MediaWiki::API ;
+use Getopt::Long;
 
-# general logic:
-#
-# retrieve the wiki page in JSON format
-#
+my %specs = (
+    "aria11" => { 
+      title => "ARIA_1.1_Testable_Statements",
+      specURL => "https://www.w3.org/TR/wai-aria11"
+    }
+);
 
 my @apiNames = qw(UIA MSAA ATK IAccessible2 AXAPI);
 
-my $dir = "../raw";
+# the suffix to attach to the automatically generated test case names
+my $theSuffix = "-manual.html";
 
-my $page = "ARIA_1.1_Testable_Statements";
+# dir is determined based upon the short name of the spec and is defined
+# by the input or on the command line
+
+my $file = undef ;
+my $spec = undef ;
+my $wiki_title = undef ;
+my $dir = undef;
+
+my $result = GetOptions(
+    "f|file=s"   => \$file,
+    "w|wiki=s"   => \$wiki_title,
+    "s|spec=s"   => \$spec,
+    "d|dir=s"   => \$dir);
 
 my $wiki_config = {
   "api_url" => "https://www.w3.org/wiki/api.php"
 };
 
-my $MW = MediaWiki::API->new( $wiki_config );
+if (!$dir) {
+  $dir = "../raw";
+}
 
-my $page = $MW->get_page( { title => $page } );
+if (!-d $dir) {
+  print STDERR "No such directory: $dir\n";
+  exit 1;
+}
 
-my $theContent = $page->{'*'};
+my $io ;
+our $theSpecURL = "";
+
+if ($spec) {
+  $wiki_title = $specs{$spec}->{title};
+  $theSpecURL = $specs{$spec}->{specURL};
+}
+
+if ($wiki_title) {
+  my $MW = MediaWiki::API->new( $wiki_config );
+  my $page = $MW->get_page( { title => $wiki_title } );
+  my $theContent = $page->{'*'};
+  $io = IO::String->new($theContent);
+} elsif ($file) {
+  open($io, "<", $file) || die("Failed to open $file: " . $@);
+} else {
+  usage() ;
+}
 
 # Now let's walk through the content and build a test page for every item
 #
 
 # iterate over the content
 
-my $io = IO::String->new($theContent);
 # my $io ;
 # open($io, "<", "raw") ;
 
 my $state = 0;   # between items
 my $current = "";
 my $theCode = "";
+my $theAttributes = {};
 my $theAsserts = {} ;
 my $theAssertCount = 0;
 my $theAPI = "";
 my $typeRows = 0;
 my $theType = "";
+my $theName = "";
+my $theRef = "";
 
 while (<$io>) {
   # look for state
+  if (m/^SpecURL: (.*)$/) {
+    $theSpecURL = $1;
+    $theSpecURL =~ s/^ *//;
+    $theSpecURL =~ s/ *$//;
+  }
   if (m/^=== (.*) ===/) {
     if ($state != 0) {
       # we were in an item; dump it
-      build_test($current, $theCode, $theAsserts) ;
+      build_test($current, $theAttributes, $theCode, $theAsserts) ;
       print "Finished $current\n";
     }
     $state = 1;
     $current = $1;
+    $theAttributes = {} ;
     $theCode = "";
     $theAsserts = {};
+    $theName = "";
   }
   if ($state == 1) {
     if (m/<pre>/) {
       # we are now in the code block
       $state = 2;
+      next;
+    } elsif (m/==== +(.*) +====/) {
+      # we are in some other block
+      $theName = lc($1);
+      $theAttributes->{$theName} = "";
+      next;
+    } 
+    if (m/^Reference: +(.*)$/) {
+      $theAttributes->{reference} = $theSpecURL . "#" . $1;
+    } elsif ($theName ne "") {
+      # accumulate whatever was in the block under the data for it
+      chomp();
+      $theAttributes->{$theName} .= $_;
     }
   }
   if ($state == 2) {
@@ -145,7 +209,7 @@ while (<$io>) {
 };
 
 if ($state != 0) {
-  build_test($current, $theCode, $theAsserts) ;
+  build_test($current, $theAttributes, $theCode, $theAsserts) ;
   print "Finished $current\n";
 }
 
@@ -154,6 +218,7 @@ exit 0;
 
 sub build_test() {
   my $title = shift ;
+  my $attrs = shift ;
   my $code = shift ;
   my $asserts = shift;
 
@@ -161,6 +226,8 @@ sub build_test() {
     print "No name provided!";
     return;
   }
+
+  my $title_reference = $title;
 
   if ($code eq "") {
     print "No code for $title; skipping.\n";
@@ -240,7 +307,30 @@ sub build_test() {
   }
 
 
-  my $asserts_json = to_json($asserts, { pretty => 1, utf8 => 1});
+  my $testDef = 
+    { "title" => $title,
+      "steps" => [
+        {
+          "type"=>  "test",
+          "title"=> "step 1",
+          "element"=> "test",
+          "test" => $asserts
+        }
+    ]
+  };
+
+  if (scalar(keys(%$attrs))) {
+    while (my $key = each(%$attrs)) {
+      print "Copying $key \n";
+      $testDef->{$key} = $attrs->{$key};
+    }
+  }
+
+  if (exists $attrs->{reference}) {
+    $title_reference = "<a href='" . $attrs->{reference} . "'>" . $title_reference . "</a>" ;
+  }
+
+  my $testDef_json = to_json($testDef, { pretty => 1, utf8 => 1});
 
   my $fileName = $title;
   $fileName =~ s/\s*$//;
@@ -248,13 +338,14 @@ sub build_test() {
   $fileName =~ s/\///g;
   $fileName =~ s/\s+/_/g;
   $fileName =~ s/=/_/g;
-  $fileName .= "-manual.html";
+  $fileName .= $theSuffix;
 
   my $template = qq(<!doctype html>
 <html>
 <head>
 <title>$title</title>
 <link rel="stylesheet" href="/resources/testharness.css">
+<link rel="stylesheet" href="/wai-aria/scripts/manual.css">
 <script src="/resources/testharness.js"></script>
 <script src="/resources/testharnessreport.js"></script>
 <script src="/wai-aria/scripts/ATTAcomm.js"></script>
@@ -262,21 +353,12 @@ sub build_test() {
 setup({explicit_timeout: true, explicit_done: true });
 
 var theTest = new ATTAcomm(
-    { "title": '$title',
-      "steps": [
-        {
-          "type":  "test",
-          "title": "step 1",
-          "element": "test",
-          "test" : $asserts_json
-        }
-    ]
-  }
+$testDef_json
 ) ;
 </script>
 </head>
 <body>
-<p>This test examines the ARIA properties for $title.</p>
+<p>This test examines the ARIA properties for $title_reference.</p>
 $code
 <div id="ATTAmessages"></div>
 <div id="manualMode"></div>
@@ -296,4 +378,18 @@ $code
   return;
 }
 
+sub usage() {
+  print STDERR q(usage: make_tests.pl -f file | -w wiki_title | -s spec [-n -v -d dir ]
 
+  -s specname   - the name of a spec known to the system
+  -w wiki_title - the TITLE of a wiki page with testable statements
+  -f file       - the file from which to read
+
+  -n            - do nothing
+  -v            - be verbose
+  -d dir        - put generated tests in directory dir
+  );
+  exit 1;
+}
+
+# vim: ts=2 sw=2 ai:
